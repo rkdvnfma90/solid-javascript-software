1f8c7a0cf869f240541bae2e26ad0fb6ef223b7d
aB <rkdvnfma90@hanmail.net>
2021-03-26 14:50:06 +0900

    feat 클릭이벤트 테스트

--- src/ClickCounter/ClickCounterView.js
+++ src/ClickCounter/ClickCounterView.js
@@ -2,10 +2,12 @@ import { App as ClickCounterApp } from './ClickCounter.js'
 
 var App = ClickCounterApp || {}
 
-App.ClickCounterView = (clickCounter, $update) => {
+App.ClickCounterView = (clickCounter, { $update, $trigger }) => {
   if (!clickCounter) throw new Error('클릭카운터를 주입받지 않았습니다.')
   if (!$update) throw new Error('update 엘리먼트를 주입받지 않았습니다.')
-  return {
+  if (!$trigger) throw new Error('trigger 엘리먼트를 주입받지 않았습니다.')
+
+  const view = {
     updateView() {
       $update.innerHTML = clickCounter.getValue()
     },
@@ -14,6 +16,12 @@ App.ClickCounterView = (clickCounter, $update) => {
       this.updateView()
     },
   }
+
+  $trigger.addEventListener('click', () => {
+    view.increaseAndUpdateView()
+  })
+
+  return view
 }
 
 export { App }
--- src/ClickCounter/ClickCounterView.test.js
+++ src/ClickCounter/ClickCounterView.test.js
@@ -13,23 +13,24 @@ import { App } from './ClickCounterView.js'
  * 하나의 기능단위로 모듈을 분리할 수 있기 때문에 단일 책임의 원칙을 지킬 수 있다.
  */
 describe('클릭카운터 뷰', () => {
-  let ClickCounter, $update, view
+  let ClickCounter, $update, $trigger, view
   beforeEach(() => {
     ClickCounter = App.ClickCounter()
     $update = document.createElement('span')
-    view = App.ClickCounterView(ClickCounter, $update)
+    $trigger = document.createElement('button') // 클릭 이벤트를 바인당할 엘리먼트
+    view = App.ClickCounterView(ClickCounter, { $update, $trigger })
   })
 
   it('ClickCounter를 주입하지 않으면 에러를 던진다.', () => {
     const clickCounter = null
     const $update = document.createElement('span')
-    const actual = () => App.ClickCounterView(clickCounter, $update)
+    const actual = () => App.ClickCounterView(clickCounter, { $update })
     expect(actual).toThrowError()
   })
 
   it('$update를 주입하지 않으면 에러를 던진다.', () => {
     const $update = null
-    const actual = () => App.ClickCounterView(ClickCounter, $update)
+    const actual = () => App.ClickCounterView(ClickCounter, { $update })
     expect(actual).toThrowError()
   })
 
@@ -56,5 +57,16 @@ describe('클릭카운터 뷰', () => {
       view.increaseAndUpdateView()
       expect(view.updateView).toHaveBeenCalled()
     })
+  })
+
+  it('클릭 이벤트가 발생하면 increaseAndUpdateView가 실행한다.', () => {
+    // 클릭카운터 뷰 모듈의 increaseAndUpdateView 함수를 감시한다.
+    spyOn(view, 'increaseAndUpdateView')
+
+    // 여기서는 클릭 이벤트를 감지해야 하는데, 카운터 값을 출력할 돔 엘리먼트를 주입했던 것처럼
+    // 클릭 이벤트 핸들러를 바인딩할 돔을 주입하자.
+    $trigger.click()
+
+    expect(view.increaseAndUpdateView).toHaveBeenCalled()
   })
 })
